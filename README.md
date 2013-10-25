c5e76d0dcd1a7c9b8f027506ac09a612e00ef967
Rory Ye <rory.cn@gmail.com>
2013-10-25 17:56:46 +0800

    change the README.md

--- README.md
+++ README.md
@@ -3,6 +3,7 @@ weixin-mp-sdk
 
 微信公众平台SDK，获取用户信息、主动发送信息...
 
+将src/test/resources/下的 mp.properties.example 文件拷贝成 mp.properties 修改里面的用户名和密码进行测试
 
 ### 补充 ###
-QQ交流群：328171904。
+fork 自 https://github.com/belerweb/weixin-mp-sdk
--- src/main/java/com/belerweb/weixin/mp/WeixinMP.java
+++ src/main/java/com/belerweb/weixin/mp/WeixinMP.java
@@ -1,7 +1,5 @@
 package com.belerweb.weixin.mp;
 
-import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.UnsupportedEncodingException;
@@ -26,11 +24,8 @@ import org.apache.http.client.methods.HttpPost;
 import org.apache.http.client.methods.HttpRequestBase;
 import org.apache.http.client.params.ClientPNames;
 import org.apache.http.client.params.CookiePolicy;
-import org.apache.http.entity.mime.FormBodyPart;
 import org.apache.http.entity.mime.MultipartEntity;
 import org.apache.http.entity.mime.content.ByteArrayBody;
-import org.apache.http.entity.mime.content.FileBody;
-import org.apache.http.entity.mime.content.InputStreamBody;
 import org.apache.http.impl.client.DefaultHttpClient;
 import org.apache.http.message.BasicNameValuePair;
 import org.apache.http.params.BasicHttpParams;
